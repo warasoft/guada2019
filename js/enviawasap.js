63572c6af8b544fc7a045dae4cb34458d80a6604
warasoft <45013513+warasoft@users.noreply.github.com>
2019-05-08 15:02:12 -0300

    Add files via upload

---
+++ js/enviawasap.js
@@ -0,0 +1,15 @@
+function copiarDatos(){
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var telefono = document.getElementById("telefono").value;
+  var consulta = document.getElementById("consulta").value;
+
+  var texto = "Nombre: "+ name + " // Email: " + email + " // Telefono: " + telefono + " // Consulta: " + consulta;
+
+  document.getElementById("textToEncode").innerHTML = texto;
+  
+  var url = "https://api.whatsapp.com/send?phone=5493875836849&text="+encodeURIComponent(texto);
+						
+  window.open(url);
+}
